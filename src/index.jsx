45fc99401bcd30bc99200cf3da79a3e593ba5374
DanLuk <danyalukashak@gmail.com>
2022-04-20 21:21:40 +0300

    prettier deleted

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 
+const [a, b] = [5, 6];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
