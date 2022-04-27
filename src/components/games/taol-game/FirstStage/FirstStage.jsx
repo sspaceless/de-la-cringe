15c479534be28fffc925fd24acc0111c64cfaa0d
Illia Bodnar <bodnarillia26@gmail.com>
2022-04-27 11:48:55 +0300

    added input to firststage component

--- src/components/games/taol-game/FirstStage/FirstStage.jsx
+++ src/components/games/taol-game/FirstStage/FirstStage.jsx
@@ -1,15 +1,36 @@
 import propTypes from 'prop-types';
+import useInput from '../../../../hooks/use-input';
 
 // eslint-disable-next-line no-unused-vars
 function FirstStage({ roomId, roomState }) {
   const { questions, players, clientId } = roomState;
-
   const { questionId } = players.find((player) => player.id === clientId);
   const { personalQuestion } = questions.find((question) => question.id === questionId);
+
+  const {
+    value: answer,
+    hasError: answerHasError,
+    isValid: isAnswerValid,
+    valueChangeHandler: inputChangeHandler,
+    inputBlurHandler
+  } = useInput((value) => value.trim().length > 0);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div>
       <h2>{personalQuestion}</h2>
+      <form onSubmit={formSubmitHandler}>
+        <input
+          maxLength="100"
+          maxvalue={answer}
+          onChange={inputChangeHandler}
+          onBlur={inputBlurHandler}
+        />
+        <button type="submit">Send!</button>
+      </form>
     </div>
   );
 }
