e63f3bc3cfb6427c2b0847e78c7eed7ce9d2dfc7
DanLuk <danyalukashak@gmail.com>
2022-06-18 03:08:34 +0300

    env variables added

--- src/api-server/config.js
+++ src/api-server/config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const Config = {
-  CLIENT_URL: 'http://localhost:3000',
-  API_URL: 'http://localhost:3002'
+  CLIENT_URL: process.env.CLIENT_URL,
+  API_URL: process.env.API_URL
 };
 
 module.exports = { default: Config };
--- src/server/src/config.js
+++ src/server/src/config.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const CROCODILE_ID = 'crocodile';
 export const TAOL_ID = 'taol';
 export const ROCKET_GAME_ID = 'my-game';
-export const API_URL = 'https://localhost:3000';
+export const API_URL = process.env.API_URL;
